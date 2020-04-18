9c7a9699ee71fd7e6afd7e9f9c2068e0eca8e280
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-04-19 01:16:45 +0200

    remove getAllProducts and added add button

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,62 @@
 <template>
   <div class="">
     <ul>
-      <li v-for="product in products" :key="product.name">
+      <li v-for="product in products" :key="product.id">
         {{ product.name }}
       </li>
     </ul>
+    <form @submit.prevent="addProduct">
+      <label for="">Name</label>
+      <input type="text" v-model="name" required />
+      <label for="">Description</label>
+      <textarea cols="30" rows="10" v-model="description"></textarea>
+      <button>Add</button>
+    </form>
   </div>
 </template>
 
 <script>
-import firestore from "./firestore";
+import firestore from './firestore';
 export default {
-  name: "App",
+  name: 'App',
   components: {},
   data: () => ({
-    products: []
+    products: [],
+    name: '',
+    description: '',
   }),
   mounted() {
-    this.getAllProducts();
+    this.detectChanges();
   },
   methods: {
-    getAllProducts() {
-      firestore
-        .collection("products")
-        .get()
-        .then(querySnapshots => {
-          this.products = querySnapshots.docs.map(doc => doc.data());
+    addProduct() {
+      if (this.name) {
+        const product = {
+          name: this.name,
+          description: this.description,
+          bought: false,
+        };
+        firestore
+          .collection('products')
+          .add(product)
+          .then((docRef) => {
+            this.name = '';
+            this.description = '';
+            console.log(docRef);
+          })
+          .catch((error) => console.log(error));
+      }
+    },
+    detectChanges() {
+      firestore.collection('products').onSnapshot((querySnapshot) => {
+        this.products = querySnapshot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          };
         });
-    }
-  }
+      });
+    },
+  },
 };
 </script>
